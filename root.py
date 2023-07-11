91f1ce17417cfaad72ba003b8a0e59ae301d5062
Daniel <daniel.mogilny322@gmail.com>
2023-07-11 13:37:34 +0300

    close button added

--- root.py
+++ root.py
@@ -1,10 +1,24 @@
-from tkinter import Tk
+from tkinter import *
 
 
 window = Tk()
 
 window.title("Pass Keeper")
 
-window.geometry("250x150")
+x_coordinate = 500
+
+y_coordinate = 400
+
+window.geometry(f"{x_coordinate}x{y_coordinate}")
+
+close_button = Button(window, text="Close", font=("Lora", 13), command=window.destroy)
+
+close_button_width = 170
+
+close_button_height = 30
+
+close_button_x = x_coordinate / 2 - close_button_width / 2
+
+close_button.place(x=close_button_width, y=y_coordinate - 50, width=close_button_width, height=close_button_height)
 
 window.mainloop()
