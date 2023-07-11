316aa8ef33e49706f54e6fd18a4662defedbe550
Daniel <daniel.mogilny322@gmail.com>
2023-07-11 14:19:35 +0300

    Password addition partually added

--- root.py
+++ root.py
@@ -5,11 +5,46 @@ window = Tk()
 
 window.title("Pass Keeper")
 
-x_coordinate = 500
+
+def add_password():
+
+    global saved_password
+
+    saved_password = password_field.get()
+
+    window.destroy()
+
+
+x_coordinate = 600
 
 y_coordinate = 400
 
 window.geometry(f"{x_coordinate}x{y_coordinate}")
+
+window.resizable(False, False)
+
+
+saved_password = ""
+
+
+password_field_label = Label(master=window, text="Enter your password:")
+
+password_field_label.configure(font=("Times New Roman", 12))
+
+password_field_label.place(x=x_coordinate - 200, y=30)
+
+
+password_field = Entry(master=window, width=20)
+
+password_field.place(x=x_coordinate - 200, y=50)
+
+
+button_add_password = Button(master=window, text="Add")
+
+button_add_password.config(command=add_password)
+
+button_clear_field = Button(master=window, text="Clear")
+
 
 close_button = Button(window, text="Close", font=("Lora", 13), command=window.destroy)
 
