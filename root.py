76b58fc9ec300274ecd3e7adeb0b6116c58e1ea9
Daniel <daniel.mogilny322@gmail.com>
2023-07-16 21:43:19 +0300

    design improved and main functionality implemented

--- root.py
+++ root.py
@@ -8,11 +8,15 @@ window.title("Pass Keeper")
 
 def add_password():
 
-    global saved_password
+    password_list.insert(END, password_field.get())
 
-    saved_password = password_field.get()
 
-    window.destroy()
+
+def get_password(*args):
+
+    password_value = password_list.get(password_list.curselection())
+
+    password.set(password_value)
 
 
 x_coordinate = 600
@@ -24,46 +28,76 @@ window.geometry(f"{x_coordinate}x{y_coordinate}")
 window.resizable(False, False)
 
 
-saved_password = ""
+frame_password_list = Frame(master=window, bd=3, relief=GROOVE)
+
+frame_password_list.place(x=5, y=5, width=x_coordinate / 2, height=y_coordinate - 10)
+
+frame_password_add = Frame(master=window, bd=3, relief=GROOVE)
+
+frame_password_add.place(x=x_coordinate / 2, y=5, width=x_coordinate / 2, height=y_coordinate - 10)
 
 
-password_field_label = Label(master=window, text="Enter your password:")
+saved_passwords = []
+
+
+password_list = Listbox(master=frame_password_list, listvariable=saved_passwords, selectmode=SINGLE)
+
+password_list.configure(bg="gray96", selectbackground="gray")
+
+password_list.configure(activestyle="none", height=4)
+
+for password in saved_passwords:
+
+    password_list.insert(END, password)
+
+password_list.select_set(0)
+
+password_list.bind("<<ListboxSelect>>", get_password)
+
+password_list.place(x=5, y=5, width = x_coordinate / 2 - 10, height=y_coordinate - 10)
+
+
+
+password_field_label = Label(master=frame_password_add, text="Enter your password:")
 
 password_field_label.configure(font=("Times New Roman", 12))
 
-password_field_label.place(x=x_coordinate - 200, y=30)
+password_field_label.place(x=5, y=5)
 
 
-password_field = Entry(master=window, width=20)
+password = StringVar()
 
-password_field.place(x=x_coordinate - 200, y=50)
+password_field = Entry(master=frame_password_add, width=20, textvariable=password)
+
+password_field.place(x=5, y=25)
 
 
-button_add_password = Button(master=window, text="Add")
+button_add_password = Button(master=frame_password_add, text="Add")
 
 button_add_password.config(command=add_password)
 
-button_add_password.place(x=x_coordinate - 150, y=70, width=50, height=30)
+button_add_password.place(x=5, y=45, width=50, height=25)
 
 
-button_clear_field = Button(master=window, text="Clear")
+button_clear_field = Button(master=frame_password_add, text="Clear")
 
-button_clear_field.place(x=x_coordinate - 80, y=70, width=50, height=30)
+button_clear_field.place(x=60, y=45, width=50, height=25)
 
 
-close_button = Button(window, text="Close", font=("Lora", 13), command=window.destroy)
+close_button = Button(frame_password_add, text="Close", font=("Lora", 13), command=window.destroy)
 
-close_button_width = 170
+close_button_width = 150
 
 close_button_height = 30
 
-close_button_x = x_coordinate / 2 - close_button_width / 2
+close_button_x = x_coordinate / 2 - 200
 
-close_button.place(x=close_button_width, y=y_coordinate - 50, width=close_button_width, height=close_button_height)
+close_button.place(x=close_button_x, y=y_coordinate - 50, width=close_button_width, height=close_button_height)
 
 window.mainloop()
 
-if saved_password != "":
+
+if len(saved_passwords) > 0:
 
     window_message = Tk()
 
