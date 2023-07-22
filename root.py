f98f1e9194dc5f278ab1edc83ba919eae5ebed9c
Daniel <daniel.mogilny322@gmail.com>
2023-07-22 18:33:13 +0300

    Revert "passwords addition and visualisation added"
    This reverts commit b53e1b71a105002559bfc065849709e159a9ce38.

--- root.py
+++ root.py
@@ -11,12 +11,8 @@ def add_password():
 
     def save_new_password():
 
-        saved_passwords_names.append(password_name.get())
-
-        password_list.insert(END, password_name.get())
-
-        saved_passwords.append(password_field.get())
-
+        password_list.insert(END, password_field.get())
+        
 
         window_new_password.destroy()
 
@@ -65,30 +61,18 @@ def add_password():
     window_new_password.resizable(False, False)
 
 
-    password_name_label = Label(master=window_new_password, text="Enter your password name(usually it's a domain address):")
-
-    password_name_label.configure(font=("Times New Roman", 12))
-
-    password_name_label.place(x=5, y=5)
-
-
-    password_name = Entry(master=window_new_password, width=20)
-
-    password_name.place(x=5, y=25)
-
-
     password_field_label = Label(master=window_new_password, text="Enter your password:")
 
     password_field_label.configure(font=("Times New Roman", 12))
 
-    password_field_label.place(x=5, y=40)
+    password_field_label.place(x=5, y=5)
 
 
     password = StringVar()
 
     password_field = Entry(master=window_new_password, width=20, textvariable=password)
 
-    password_field.place(x=5, y=65)
+    password_field.place(x=5, y=25)
 
 
     button_save_password = Button(master=window_new_password, text="Save")
@@ -106,11 +90,9 @@ def add_password():
 
 def get_password(*args):
 
-    password_name_value = password_list.get(password_list.curselection())
+    password_value = password_list.get(password_list.curselection())
 
-    password_value_index = saved_passwords_names.index(password_name_value)
-
-    password.set(saved_passwords[password_value_index])
+    password.set(password_value)
 
 
 
@@ -132,21 +114,20 @@ frame_password_add = Frame(master=window, bd=3, relief=GROOVE)
 frame_password_add.place(x=x_coordinate / 2, y=5, width=x_coordinate / 2, height=y_coordinate - 10)
 
 
-saved_passwords_names = ["google.com",]
-
-saved_passwords = ["19012000",]
+saved_passwords = []
 
 
-password_list = Listbox(master=frame_password_list, listvariable=saved_passwords_names, selectmode=SINGLE)
+password_list = Listbox(master=frame_password_list, listvariable=saved_passwords, selectmode=SINGLE)
 
 password_list.configure(bg="gray96", selectbackground="gray")
 
 password_list.configure(activestyle="none", height=4)
 
-for password in saved_passwords_names:
+for password in saved_passwords:
 
     password_list.insert(END, password)
 
+password_list.select_set(0)
 
 password_list.bind("<<ListboxSelect>>", get_password)
 
@@ -169,7 +150,7 @@ password_field_label.place(x=5, y=40)
 
 password = StringVar()
 
-password_field = Entry(master=frame_password_add, width=20, show="*", textvariable=password)
+password_field = Entry(master=frame_password_add, width=20, textvariable=password)
 
 password_field.place(x=5, y=60)
 
