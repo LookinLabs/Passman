6f8ed1fcafb19f387053da925fd76de4f34e3bfe
Daniel <daniel.mogilny322@gmail.com>
2023-07-23 18:51:24 +0300

    added autopep8 formating with modifications
    If arguments are too long for autopep8, they all should be moved to the next line. There are 2 empty lines should be added after every interface component or logical functionality

--- root.py
+++ root.py
@@ -29,7 +29,7 @@ def add_password():
 
         window_new_password.destroy()
 
-    
+
         window_message = Tk()
 
         window_message.title("Notification")
@@ -43,11 +43,12 @@ def add_password():
         window_message.resizable(False, False)
 
 
-        message_label = Label(master=window_message, text="Password successfully added!", relief=GROOVE)
+        message_label = Label(
+            master=window_message, text="Password successfully added!", relief=GROOVE)
 
         message_label_width = 300
 
-        message_label.place(x=window_message_x /  2 - message_label_width / 2, y=10, width=message_label_width, height=40)
+        message_label.place(x=window_message_x / 2 - message_label_width / 2, y=10, width=message_label_width, height=40)
 
 
         message_button = Button(master=window_message, text="Ok")
@@ -56,7 +57,8 @@ def add_password():
 
         message_button_width = 70
 
-        message_button.place(x=window_message_x /  2 - message_button_width / 2, y=100, width=message_button_width, height=45)
+        message_button.place(x=window_message_x / 2 - message_button_width / 2, y=100, width=message_button_width, height=45)
+
 
         window_message.mainloop()
 
@@ -69,12 +71,14 @@ def add_password():
 
     window_new_password_y = 200
 
-    window_new_password.geometry(f"{window_new_password_x}x{window_new_password_y}")
+    window_new_password.geometry(
+        f"{window_new_password_x}x{window_new_password_y}")
 
     window_new_password.resizable(False, False)
 
 
-    password_name_label = Label(master=window_new_password, text="Enter your password's name(usually it's a domain address):")
+    password_name_label = Label(
+        master=window_new_password, text="Enter your password's name(usually it's a domain address):")
 
     password_name_label.configure(font=("Times New Roman", 12))
 
@@ -86,7 +90,8 @@ def add_password():
     password_name.place(x=5, y=25)
 
 
-    password_field_label = Label(master=window_new_password, text="Enter your password:")
+    password_field_label = Label(
+        master=window_new_password, text="Enter your password:")
 
     password_field_label.configure(font=("Times New Roman", 12))
 
@@ -105,14 +110,17 @@ def add_password():
     button_save_password.place(x=5, y=95, width=50, height=25)
 
 
-    button_clear_field = Button(master=window_new_password, text="Cancel", command=window_new_password.destroy)
+    button_clear_field = Button(
+        master=window_new_password, text="Cancel", command=window_new_password.destroy)
 
     button_clear_field.place(x=60, y=95, width=50, height=25)
 
 
-    button_show_password = Button(window_new_password, text="Show", command=show_and_hide_password)
+    button_show_password = Button(
+        window_new_password, text="Show", command=show_and_hide_password)
 
-    button_show_password.place(x=window_new_password_x / 3 * 2, y=70, width=50, height=25)
+    button_show_password.place(
+        x=window_new_password_x / 3 * 2, y=70, width=50, height=25)
 
 
 def get_password(*args):
@@ -152,17 +160,20 @@ def delete_password(*args):
 
     window_delete_password_y = 200
 
-    window_delete_password.geometry(f"{window_delete_password_x}x{window_delete_password_y}")
+    window_delete_password.geometry(
+        f"{window_delete_password_x}x{window_delete_password_y}")
 
     window_delete_password.resizable(False, False)
 
 
-    label_delete_password = Label(master=window_delete_password, text=f"Are you sure you want to delete password '{saved_passwords_names[password_value_index]}'?", relief=GROOVE)
+    label_delete_password = Label(
+        master=window_delete_password, text=f"Are you sure you want to delete password '{saved_passwords_names[password_value_index]}'?", relief=GROOVE)
 
-    label_delete_password_width = 370 
+    label_delete_password_width = 370
 
-    label_delete_password.place(x=window_delete_password_x /  2 - label_delete_password_width / 2, y=10, width=label_delete_password_width, height=40)
-
+    label_delete_password.place(
+        x=window_delete_password_x / 2 - label_delete_password_width / 2, y=10, width=label_delete_password_width, height=40)
+    
 
     button_save_password = Button(master=window_delete_password, text="Yes!")
 
@@ -178,7 +189,6 @@ def delete_password(*args):
     button_save_password.place(x=70, y=95, width=50, height=25)
 
 
-
 x_coordinate = 600
 
 y_coordinate = 400
@@ -190,11 +200,14 @@ window.resizable(False, False)
 
 frame_password_list = Frame(master=window, bd=3, relief=GROOVE)
 
-frame_password_list.place(x=5, y=5, width=x_coordinate / 2 - 20, height=y_coordinate - 10)
+frame_password_list.place(
+    x=5, y=5, width=x_coordinate / 2 - 20, height=y_coordinate - 10)
+
 
 frame_password_add = Frame(master=window, bd=3, relief=GROOVE)
 
-frame_password_add.place(x=x_coordinate / 2, y=5, width=x_coordinate / 2, height=y_coordinate - 10)
+frame_password_add.place(
+    x=x_coordinate / 2, y=5, width=x_coordinate / 2, height=y_coordinate - 10)
 
 
 saved_passwords_names = []
@@ -215,10 +228,12 @@ for password in saved_passwords_names:
 
 password_list.bind("<<ListboxSelect>>", get_password)
 
-password_list.place(x=5, y=5, width = x_coordinate / 2 - 40, height=y_coordinate - 30)
+password_list.place(
+    x=5, y=5, width=x_coordinate / 2 - 40, height=y_coordinate - 30)
 
 
-button_add_password = Button(master=frame_password_add, text="Add new password")
+button_add_password = Button(
+    master=frame_password_add, text="Add new password")
 
 button_add_password.config(command=add_password)
 
@@ -234,7 +249,8 @@ password_field_label.place(x=5, y=40)
 
 password = StringVar()
 
-password_field = Entry(master=frame_password_add, width=20, show="*", textvariable=password, state=DISABLED)
+password_field = Entry(
+    master=frame_password_add, width=20, show="*", textvariable=password, state=DISABLED)
 
 password_field.place(x=5, y=60)
 
@@ -246,7 +262,8 @@ button_delete_password.config(command=delete_password)
 button_delete_password.place(x=200, y=60)
 
 
-close_button = Button(frame_password_add, text="Close", font=("Lora", 13), command=window.destroy)
+close_button = Button(
+    frame_password_add, text="Close", font=("Lora", 13), command=window.destroy)
 
 close_button_width = 150
 
@@ -254,6 +271,8 @@ close_button_height = 30
 
 close_button_x = x_coordinate / 2 - 200
 
-close_button.place(x=close_button_x, y=y_coordinate - 50, width=close_button_width, height=close_button_height)
+close_button.place(
+    x=close_button_x, y=y_coordinate - 50, width=close_button_width, height=close_button_height)
+
 
 window.mainloop()
