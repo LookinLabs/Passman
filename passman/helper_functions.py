b4d717b89b24dd446c450906b485deacaf47d8d1
dan-i-el <maysonb901@gmail.com>
2023-09-23 21:38:05 +0300

    password validation functions added

--- passman/helper_functions.py
+++ passman/helper_functions.py
@@ -7,3 +7,225 @@ def toggle_password(entry):
     else:
 
         entry["show"] = "*"
+
+
+# """Password validation."""
+
+
+# def is_correct_length(password: str) -> bool:
+#     """
+#     Check if the password's length is within the valid range.
+
+#     The password should have a length between 8 and 64 symbols.
+#     :param password: Password to be checked
+#     :return: True if the password's length is within the valid range, False otherwise
+#     """
+#     password_length = len(password)
+
+#     return password_length >= 8 and password_length <= 64
+
+
+# def includes_uppercase(password: str) -> bool:
+#     """
+#     Check if the password contains at least one uppercase letter.
+
+#     :param password: Password to be checked
+#     :return: True if the password contains at least one uppercase letter, False otherwise
+#     """
+#     for symbol in password:
+
+#         if symbol.isupper():
+
+#             return True
+
+#     return False
+
+
+# def includes_lowercase(password: str) -> bool:
+#     """
+#     Check if the password contains at least one lowercase letter.
+
+#     :param password: Password to be checked
+#     :return: True if the password contains at least one lowercase letter, False otherwise
+#     """
+#     for symbol in password:
+
+#         if symbol.islower():
+
+#             return True
+
+#     return False
+
+
+# def includes_special(password: str) -> bool:
+#     """
+#     Check if the password contains at least one special character (whitespace is also considered a special character).
+
+#     :param password: Password to be checked
+#     :return: True if the password contains at least one special character, False otherwise
+#     """
+#     for symbol in password:
+
+#         if not symbol.isalnum():
+
+#             return True
+
+#     return False
+
+
+# def includes_number(password: str) -> bool:
+#     """
+#     Check if the password contains at least one numeric digit.
+
+#     :param password: Password to be checked
+#     :return: True if the password contains at least one numeric digit, False otherwise
+#     """
+#     for symbol in password:
+
+#         if symbol.isdigit():
+
+#             return True
+
+#     return False
+
+
+# def is_different_from_old_password(old_pass: str, new_pass: str) -> bool:
+#     """
+#     Check if the new password is different enough from the old password.
+
+#     The overlap between the new password and old password should be less than 50%.
+#     The check for overlap is case-insensitive.
+#     The overlap is also checked for the reversed version of the new password.
+
+#     :param old_pass: The old password
+#     :param new_pass: The new password
+#     :return: True if the new password is different enough, False otherwise
+#     """
+#     new_pass_lowercase = new_pass.lower()
+
+#     new_password_length = len(new_pass_lowercase)
+
+#     half_of_old_password = len(old_pass) // 2
+
+#     for index in range(new_password_length - half_of_old_password + 1):
+
+#         end_index = index + half_of_old_password
+
+#         substring = new_pass_lowercase[index:end_index]
+
+#         if substring in old_pass or substring[::-1] in old_pass:
+
+#             return False
+
+#     return True
+
+
+# def is_name_in_password(password: str, name: str) -> bool:
+#     """
+#     Check if the password contains the name of the account owner.
+
+#     The name received as input may contain whitespace to separate the first and last name, neither of which should be
+#     present in the password.
+#     If the name contains a hyphen (such as Mari-Liis), neither part of the name should be present in the password.
+#     The name should not be in the password even if the casing of it is different in the password.
+#     Reversed format of the name is also not allowed in the password
+
+#     :param password: The password to be validated
+#     :param name: The full name of the account owner
+#     :return: True if the name is present in the password, False otherwise
+#     """
+#     HYPHEN = "-"
+
+#     SPACE = " "
+
+#     password_lowercase = password.lower()
+
+#     lowercase_name = name.lower()
+
+#     name_without_hyphens = lowercase_name.replace(HYPHEN, SPACE)
+
+#     name_value = name_without_hyphens.split(SPACE)
+
+#     for part in name_value:
+
+#         if part in password_lowercase or part[::-1] in password_lowercase:
+
+#             return True
+
+#     return False
+
+
+# def is_birthday_in_password(password: str, birthdate: str) -> bool:
+#     """
+#     Check if the password contains the birthday of the account owner.
+
+#     The day, month or year in the birthdate cannot be present in the password. For the birth year, the last two digits
+#     of the birth year separately is also not allowed.
+
+#     For the day, month or last 2 digits of the year, the reversed number is allowed but for the full 4-digit year is
+#     not allowed in the reverse format.
+
+#     The date is always in the format "dd.mm.yyyy", where
+#     dd is 2-digit day (01, 02, .. 31)
+#     mm is 2-digit month (01, 02, .. 12)
+#     yyyy is 4-digit year (0001, 0002, ..., 2022, 2023, ..., 3000, ...)
+
+#     You don't have to validate the date.
+
+#     :param password: The password to be validated
+#     :param birthdate: Birthday of the account owner, format is dd.mm.yyyy
+#     :return: True if the birthday is present in the password, False otherwise
+#     """
+#     birthday_data = birthdate.split(".")
+
+#     full_year = birthday_data[-1]
+
+#     if full_year[::-1] in password:
+
+#         return True
+
+#     for number in birthday_data:
+
+#         if number[-2:] in password:
+
+#             return True
+
+#     return False
+
+
+# def is_password_valid(new_password: str, old_password: str, name: str, birthdate: str) -> bool:
+#     """
+#     Check whether the given password is valid.
+
+#     This function combines several checks to determine if the provided password is valid.
+#     It checks the length, presence of uppercase and lowercase letters, inclusion of at least one number,
+#     inclusion of at least one special character, absence of the user's name and birthdate in the password.
+#     Call the functions you wrote before within this one to complete the validation.
+
+#     :param new_password: The password to be checked
+#     :param old_password: the previous password of this account
+#     :param name: The user's full name
+#     :param birthdate: The user's birthdate
+#     :return: True if the password is valid, False otherwise.
+#     """
+#     length = is_correct_length(new_password)
+
+#     uppercase = includes_uppercase(new_password)
+
+#     lowercase = includes_lowercase(new_password)
+
+#     special = includes_special(new_password)
+
+#     number = includes_number(new_password)
+
+#     difference = is_different_from_old_password(old_password, new_password)
+
+#     name_in_password = is_name_in_password(new_password, name)
+
+#     birthday = is_birthday_in_password(new_password, birthdate)
+
+#     validators = (length, uppercase, lowercase, special, number, difference)
+
+#     falsy_validators = (name_in_password, birthday)
+
+#     return all(validators) and not all(falsy_validators)
