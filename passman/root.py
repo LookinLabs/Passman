e9901893ca62cf36c3a137ad3c189f26bd52314b
dan-i-el <maysonb901@gmail.com>
2023-09-24 01:22:07 +0300

    get password function rewritten

--- passman/root.py
+++ passman/root.py
@@ -13,11 +13,6 @@ class App(tk.Tk):
         #running the app
         super().__init__()
 
-        #switch it to connection with database
-        self.saved_passwords_names = []
-
-        self.saved_passwords = []
-
 
         self.__initialize_main_window()
 
@@ -65,7 +60,6 @@ class App(tk.Tk):
 
         self.password_list = tk.Listbox(
             master=self.frame_password_list,
-            listvariable=self.saved_passwords_names,
             selectmode=tk.SINGLE,
             font=("Times New Roman", 12),
             bg="gray96",
@@ -176,16 +170,16 @@ class App(tk.Tk):
         )
 
 
-        self.button_update_password = ButtonGenerator(
-            master=self.frame_password_handling,
-            text="Edit",
-            state="disabled", 
-            command=self.update_password,
-            x=style.x_button_update_password,
-            y=style.y_button_show_password,
-            width=style.width_button,
-            height=style.height_button
-        )
+        # self.button_update_password = ButtonGenerator(
+        #     master=self.frame_password_handling,
+        #     text="Edit",
+        #     state="disabled", 
+        #     command=self.update_password,
+        #     x=style.x_button_update_password,
+        #     y=style.y_button_show_password,
+        #     width=style.width_button,
+        #     height=style.height_button
+        # )
 
 
         self.button_close_window = ButtonGenerator(
@@ -211,13 +205,15 @@ class App(tk.Tk):
 
     def __get_password(self, *args):
 
-        password_name_value = self.password_list.get(self.password_list.curselection())
+        password_list_value = self.password_list.get(self.password_list.curselection()).split(" ")
 
-        password_value_index = self.saved_passwords_names.index(password_name_value)
+        db_connection = sqlite3.connect("passman.db")
+        
+        cursor = db_connection.cursor()
+        
+        data = cursor.execute(f"SELECT password FROM passman WHERE password_name = '{password_list_value[0]}' AND password_address = '{password_list_value[1]}';")
 
-        password_string_value = self.saved_passwords[password_value_index].rstrip("\n")
-
-        self.password_value.set(password_string_value)
+        self.password_value.set(data.fetchall()[0][0])
 
         self.button_update_password.config(state="normal")
 
