84c0e02c96ec99b2d67a2dd56e66a9edd7cda6a8
Dan-I-El <maysonb901@gmail.com>
2024-02-08 15:28:03 +0200

    some methods added

--- passman/intro.py
+++ passman/intro.py
@@ -7,9 +7,13 @@ from pysqlitecypher import sqlitewrapper
 import helper_functions
 
 
+TABLE_NAME = "passman"
+
+
 class WindowWelcome(tk.Toplevel):
 
     def __init__(self):
+
 
         self.db = sqlitewrapper.SqliteCipher(
             dataBasePath="passman.db",
@@ -17,7 +21,7 @@ class WindowWelcome(tk.Toplevel):
             password=None
         )
 
-        self.db.createTable("passman",
+        self.db.createTable(TABLE_NAME,
                             [
                                 ["password_address", "TEXT"], 
                                 ["password_name", "TEXT"], 
@@ -27,6 +31,23 @@ class WindowWelcome(tk.Toplevel):
                             ],
                             makeSecure=True,
                             commit=True
-                            )
+        )
         
-        self.db.insertIntoTable(tableName , insertList , commit = True)
+        self.db.insertIntoTable(TABLE_NAME ,
+                                [
+                                    ["hh.ee", "haha", "12345678", "this password's description"],
+                                    ["asdfg.com", "asdfg", "123412", "other description"]
+                                ] ,
+                                commit=True
+        )
+
+        self.db.getDataFromTable(TABLE_NAME, raiseConversionError=False, omitID=False)
+
+        # change raiseError to the True in the future
+        self.db.deleteDataInTable(TABLE_NAME , 5 , commit = True, raiseError = False, updateId=True)
+
+
+        self.db.updateIDs(tableName , commit = True)
+
+
+        self.db.updateInTable(tableName , iDValue , colName , colValue , commit = True , raiseError = True)
